49c7e42c731f1987fbdbb127e6760ff56e4fade5
appdeve12 <appdevelopergfo508@gmail.com>
2025-07-24 17:28:54 +0530

    update

--- fcmService.js
+++ fcmService.js
@@ -1,6 +1,6 @@
 // firebase.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./woman-safety-c2b87-firebase-adminsdk-fbsvc-b332de8750.json"); // Make sure path is correct
+const serviceAccount = require("/etc/secrets/woman-safety-c2b87-firebase-adminsdk-fbsvc-b332de8750.json"); // Make sure path is correct
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
