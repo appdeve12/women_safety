325fce771186f7a28b28ab14bec15da47da04034
appdeve12 <appdevelopergfo508@gmail.com>
2025-08-04 17:50:50 +0530

    update

--- models/NotificationStatusSchema.js
+++ models/NotificationStatusSchema.js
@@ -8,7 +8,8 @@ const NotificationStatusSchema = new mongoose.Schema({
   status: { type: String, enum: ["sent", "delivered"], default: "sent" },
   sentAt: { type: Date, default: Date.now },
   deliveredAt: { type: Date },
-usernotification:{type:mongoose.Mongoose.Schema.type.objectId,ref:"Police"}
+
+  
 });
 
 module.exports = mongoose.model("NotificationStatus", NotificationStatusSchema);
